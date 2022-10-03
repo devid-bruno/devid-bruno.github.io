4dd71308ac64c3090a807722fefe49acce63b8a1
Devid Bruno <devidbruno@MacBook-Air-de-Devid.local>
2022-10-03 09:57:28 -0300

    coringuei dnv

--- database/database.js
+++ database/database.js
@@ -1,13 +1,11 @@
 const sequelize = require('sequelize');
 
-var db = 'heroku_466a2a69a79072f1'
+var db = 'heroku_466a2a69a79072f'
 
 const connect = new sequelize(`${db}`, 'b4cc72465aee31', 'f06cfbbe', {
     host: 'us-cdbr-east-06.cleardb.net',
     dialect: 'mysql',
     timezone: '-03:00'
 })
-
-//mysql://:@/?reconnect=true
 
 module.exports = connect;
