c55330a6eb642c4fb3db4784560f9a0db27a3aea
Devid Bruno <devidbruno@MacBook-Air-de-Devid.local>
2022-10-03 08:23:12 -0300

    pswd db changes

--- database/database.js
+++ database/database.js
@@ -2,7 +2,7 @@ const sequelize = require('sequelize');
 
 var db = 'heroku_65b1964ba89f8c7'
 
-const connect = new sequelize(`${db}`, 'b22f2d4798a54a', 'd0d3a947', {
+const connect = new sequelize(`${db}`, 'b22f2d4798a54a', 'coringuei', {
     host: 'us-cdbr-east-06.cleardb.net',
     dialect: 'mysql',
     timezone: '-03:00'
