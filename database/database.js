71cf5b54fb2a338563090e460dd555b6d8ab4211
Devid Bruno <devidbruno@MacBook-Air-de-Devid.local>
2022-10-03 10:00:07 -0300

    coringuei dnv aaa

--- database/database.js
+++ database/database.js
@@ -6,6 +6,10 @@ const connect = new sequelize(`${db}`, 'b4cc72465aee31', 'f06cfbbe', {
     host: 'us-cdbr-east-06.cleardb.net',
     dialect: 'mysql',
     timezone: '-03:00'
-})
+}).then(() => {
+    console.log("Conectado com sucesso!");
+}).catch((erro) => {
+    console.log("Erro ao conectar: " + erro);
+});
 
 module.exports = connect;
